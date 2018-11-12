ae07ad7b725438619ecc370883695157d9faefe8
iykeevans <elochi238@gmail.com>
2018-11-12 03:17:55 +0100

    setup express web server

---
+++ src/app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+const port = process.env.PORT || 8080;
+
+// fire express
+const app = express();
+
+// use bodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// use imported routes
+app.get('/', (req, res) => res.json({message: 'welcome to the demo of this API'}));
+
+app.listen(port, () => {
+    console.log(`listening to port ${port}`);
+});
+
+module.exports = app;
