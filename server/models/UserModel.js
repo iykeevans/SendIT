2eabc93372fe4cf1af45743503cbc9e4f398bcbb
iykeevans <elochi238@gmail.com>
2018-11-15 03:57:43 +0100

    converted user model to es6 and setup communications with database

---
+++ server/models/UserModel.js
@@ -0,0 +1,13 @@
+import db from '../database/database.json';
+import helper from '../validations/helper';
+
+const getUserParcels = (id) => {
+  return new Promise((resolve, reject) => {
+    // check if object with id exists
+    helper.checkUserExist(db, id)
+    .then(data => resolve(data))
+    .catch(err => reject(err));
+  });
+}
+
+export default { getUserParcels };
