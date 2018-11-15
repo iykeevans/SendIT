75391ff19166de0dddb1cccb289514a9e724cb4b
iykeevans <elochi238@gmail.com>
2018-11-15 03:56:31 +0100

    converted parcel model e6 and setup communications with database

---
+++ server/models/ParcelModel.js
@@ -0,0 +1,47 @@
+import db from '../database/database.json';
+import helper from '../helpers/helper';
+
+const getAllParcels = () => {
+  return new Promise((resolve, reject) => {
+    if(db.length === 0) {
+      reject({
+        message: 'No data available',
+        status: 202
+      })
+    }
+    resolve(db);
+  });
+}
+
+const getOneParcel = (id) => {
+  return new Promise((resolve, reject) => {
+    // check if object with id exists
+    helper.checkItemExist(db, id)
+    .then(data => resolve(data))
+    .catch(err => reject(err));
+  });
+}
+
+const insertParcel = (newParcel) => {
+  return new Promise((resolve, reject) => {
+    newParcel.id = //add shortId here
+    newParcel.userId = //add shorId here
+    newParcel.createdAt = helper.newDate();
+    db.push(newParcel);
+    resolve(newParcel);
+  });
+}
+
+const cancelParcel = (id) => {
+  let parcel = db.find(item => {
+    return item.id == id;
+  })
+  if(parcel.status === true) {
+    parcel.status = false
+  } else {
+    return {message: 'order is already cancelled'}
+  }
+  return parcel
+}
+
+export default { getAllParcels, getOneParcel, insertParcel, cancelParcel }
