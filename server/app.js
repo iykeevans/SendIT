8e5297d932b40b94f3855a91ae55c9a9cf23f72c
iykeevans <elochi238@gmail.com>
2018-11-17 22:09:47 +0100

    set up express [finishes #162033765]

--- server/app.js
+++ server/app.js
@@ -1,1 +1,19 @@
-console.log('app working');
+import express from 'express';
+import bodyParser from 'body-parser';
+
+// set port
+const port = process.env.PORT || 8080;
+
+// fire express function
+const app = express();
+
+// use body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// home end point
+app.get('/', (req, res) => {
+    res.json({message: 'welcome to my API'});
+});
+
+app.listen(port, () => console.log(`app listening on port ${port}`));
