435fdddbb19224413272094d7fa774e9cf088873
iykeevans <elochi238@gmail.com>
2018-11-18 19:22:27 +0100

    set up eslint [finishes #162033771]

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import log from 'fancy-log';
 
 // set port
 const port = process.env.PORT || 8080;
@@ -13,9 +14,9 @@ app.use(bodyParser.json());
 
 // home end point
 app.get('/', (req, res) => {
-    res.json({message: 'welcome to my API'});
+  res.json({ message: 'welcome to my API' });
 });
 
-app.listen(port, () => console.log(`app listening on port ${port}`));
+app.listen(port, () => log(`app listening on port ${port}`));
 
 module.exports = app;
--- server/test/test.js
+++ server/test/test.js
@@ -3,15 +3,15 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('Test for App.js', () => {
-    it('it should return success code 200', () => {
-        return request(app).get('/')
-        .then((res) => {
-            expect(res.statusCode).to.equal(200);
-            expect(res.body).to.be.an('object');
-            expect(res.body).to.have.property('message');
-        })
-        .catch((err)=> {
-            expect(err.statusCode).to.equal(404);
-        });
-    });
+  it('it should return success code 200', () => {
+    request(app).get('/')
+      .then((res) => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('message');
+      })
+      .catch((err) => {
+        expect(err.statusCode).to.equal(404);
+      });
+  });
 });
