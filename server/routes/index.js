0066c2a2f55cec7ca1e3d8788a9ebb693c4bfa19
iykeevans <elochi238@gmail.com>
2018-11-21 00:54:03 +0100

    feat api implement get user parcels endpoint [finishes #162064525]

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import parcels from './ParcelRoute';
+import users from './UserRoute';
 
 const router = express.Router();
 
 router.all('/api/v1', (req, res) => res.json({ message: 'welcome to my API' }));
 router.use('/api/v1/parcels', parcels);
+router.use('/api/v1/users', users);
 
 export default router;
