1fd8d985c24b9b6bfcd76f600034bd8d836d1b1a
Ezeani Ikenna Evan <32799580+iykeevans@users.noreply.github.com>
2018-11-20 23:54:12 +0100

    chore api add get specific parcel test

--- server/test/parcel.js
+++ server/test/parcel.js
@@ -4,12 +4,29 @@ const app = require('../app');
 
 describe('Test for All Parcel routes', () => {
   describe('Test for All GET routes', () => {
+
+    // test for all parcel delivery order
     describe('GET /parcels', () => {
       it('it should return success code 200', () => {
         request(app).get('/api/v1/parcels')
           .then((res) => {
             expect(res.statusCode).to.equal(200);
             expect(res.body).to.be.an('array');
+          })
+          .catch((err) => {
+            expect(err.statusCode).to.equal(404);
+          });
+      });
+    });
+
+    // test for specific parcel delivery order
+    describe('GET /parcels/:id', () => {
+      it('it should return success code 200', () => {
+        request(app).get('/api/v1/parcels/1')
+          .then((res) => {
+            expect(res.statusCode).to.equal(200);
+            expect(res.body).to.be.an('object');
+            expect(res.body).to.have.property('userId');
           })
           .catch((err) => {
             expect(err.statusCode).to.equal(404);
